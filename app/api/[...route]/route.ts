e4cda89d50151096a8c6c718329674c1e0be6483
Régi GOUALE <regi.gouale@gmail.com>
2025-10-16 23:48:53 +0200

    feat: integrate better-auth for user authentication and session management
    - Added user, session, account, and verification models to Prisma schema.
    - Implemented authentication logic using better-auth in the API routes.
    - Created auth client for better-auth integration.
    - Updated API to handle user sessions and authentication.
    - Modified home page to display a welcome message.
    - Updated .gitignore to exclude database and HTTP files.
    - Added necessary dependencies for better-auth and Hono framework.

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,21 +1,45 @@
-import { zValidator } from "@hono/zod-validator";
+import { auth } from "@/lib/auth";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import z from "zod";
 
-const app = new Hono().basePath("/api");
+export const runtime = "nodejs";
 
-app.get(
-  "/hello",
-  zValidator("query", z.object({ name: z.string().optional() })),
-  (c) => {
-    const { name } = c.req.valid("query");
-    return c.json({
-      message: `Hello ${name ?? "World"} from faktuur.io API!`,
-    });
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>().basePath("/api");
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
   }
-);
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
+
+app.on(["POST", "GET"], "/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
+
+app.get("/session", (c) => {
+  const session = c.get("session");
+  const user = c.get("user");
+
+  if (!user) return c.body(null, 401);
+
+  return c.json({
+    session,
+    user,
+  });
+});
 
 export const GET = handle(app);
-
 export const POST = handle(app);
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/hello`);
-  const data = await res.json();
-  const message = data.message;
-  if (!message) return <p>Loading...</p>;
-
-  return <div className="text-2xl font-bold">{message}</div>;
+  return <div className="text-2xl font-bold">Bienvenu</div>;
 }
