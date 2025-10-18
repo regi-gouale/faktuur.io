a0e06965fd2c5228acbd4b52fd6ca0c81e27150f
Régi GOUALE <regi.gouale@gmail.com>
2025-10-18 12:17:46 +0200

    ✨ feat(auth): ajouter la vérification par email lors de l'inscription
    Ajoute la fonctionnalité d'envoi d'un email de vérification
    lors de l'inscription d'un utilisateur. Cela inclut la
    configuration pour envoyer l'email avec un lien de vérification.

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI, organization } from "better-auth/plugins";
+import { sendEmailVerificationEmail } from "./dal/email";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -9,6 +10,12 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      await sendEmailVerificationEmail(user.email, url);
+    },
   },
   plugins: [organization(), openAPI()],
   session: {
